2a3a812a93854c0f04b7e208682e34c2f7012566
johnfriz+demo@gmail.com <johnfriz+demo@gmail.com>
2012-05-30 23:09:09 +0000

    update file /cloud/stock.js

--- cloud/stock.js
+++ cloud/stock.js
@@ -28,38 +28,48 @@ var stock = {
 			charset : "UTF-8",
 			period : 3600
 		}, function(err, symbolRes) {
-			//Clear up YAHOO response and only keep the information "stock symbol" we need.
-			var stockSymbol = stock.processSymbolRes(symbolRes);
-
-			// construct SOAP envelop. We could do this manually or just use a Javascript Library.
-			var soapEnvolope = '<?xml version="1.0" encoding="utf-8"?>' + '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' + '<soap:Body>' + '<GetQuote xmlns="http://www.webserviceX.NET/">' + '<symbol>' + stockSymbol + '</symbol>' + '</GetQuote>' + '</soap:Body>' + '</soap:Envelope>';
-
-			//Retrieve SOAP url
-			var stockInfoUrl = stock.webServiceXApi;
-
-			//Prepare webcall parameters
-			var opt = {
-				url : stockInfoUrl,
-				method : "POST",
-				charset : "UTF-8",
-				contentType : "text/xml",
-				body : soapEnvolope,
-				period : 3600
-			}
-
-			//Perform webcall
-			$fh.web(opt, function(err, res) {
-				var xml2js=require ("xml2js");
-				//getSOAPElement will retrieve specific XML object within SOAP response
-				(new xml2js.Parser()).parseString(res.body, function(err, jsres) {
-					var quoteRes=jsres["soap:Body"]["GetQuoteResponse"]["GetQuoteResult"];
-					//mash up the data and return to client.
-					callback(err, {
-						stockSymbol : stockSymbol,
-						stockInfo : quoteRes
-					});
-				});
-			});
+      if( err ) {
+        callback(err, null);
+      }
+      else {
+        //Clear up YAHOO response and only keep the information "stock symbol" we need.
+        var stockSymbol = stock.processSymbolRes(symbolRes);
+        
+        // construct SOAP envelop. We could do this manually or just use a Javascript Library.
+        var soapEnvolope = '<?xml version="1.0" encoding="utf-8"?>' + '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' + '<soap:Body>' + '<GetQuote xmlns="http://www.webserviceX.NET/">' + '<symbol>' + stockSymbol + '</symbol>' + '</GetQuote>' + '</soap:Body>' + '</soap:Envelope>';
+        
+        //Retrieve SOAP url
+        var stockInfoUrl = stock.webServiceXApi;
+        
+        //Prepare webcall parameters
+        var opt = {
+        	url : stockInfoUrl,
+        	method : "POST",
+        	charset : "UTF-8",
+        	contentType : "text/xml",
+        	body : soapEnvolope,
+        	period : 3600
+        }
+        
+        //Perform webcall
+        $fh.web(opt, function(err, res) {
+          if( err ) {
+            callback(err, null);
+          }
+          else {
+          	var xml2js=require ("xml2js");
+          	//getSOAPElement will retrieve specific XML object within SOAP response
+          	(new xml2js.Parser()).parseString(res.body, function(err, jsres) {
+          		var quoteRes=jsres["soap:Body"]["GetQuoteResponse"]["GetQuoteResult"];
+          		//mash up the data and return to client.
+          		callback(err, {
+          			stockSymbol : stockSymbol,
+          			stockInfo : quoteRes
+          		});
+          	});
+          }
+        });
+      }
 		});
 	},
 	/**
