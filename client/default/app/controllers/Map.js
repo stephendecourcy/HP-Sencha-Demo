41ff51846a09150aae48954617e8db63fadb0930
johnfriz+demo@gmail.com <johnfriz+demo@gmail.com>
2012-06-01 17:39:49 +0000

    update file /client/default/app/controllers/Map.js

--- client/default/app/controllers/Map.js
+++ client/default/app/controllers/Map.js
@@ -50,7 +50,7 @@ app.controllers.map = new Ext.Controller({
     }, function(res) {
       if (hash && hash === res.hash) {
         console.log("Client data is at the latest version");
-        
+        points = JSON.parse(cache);
       } else {
         points = res;
         $fh.data({
