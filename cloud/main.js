52e697a61f5288939414e56d8b7a3793649dc236
stephen.decourcy@feedhenry.com <stephen.decourcy@feedhenry.com>
2013-07-04 16:00:07 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -23,6 +23,10 @@ function payment(params,callback) {
   });
 }
 
+
+exports.nodevers = function (params, callback){
+  return callback(undefined, {"version":process.version});
+};
 /*
  * Twitter
  */
